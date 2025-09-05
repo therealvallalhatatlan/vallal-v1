a293dc7a18be4a25480379392f8d58bd54305a14
utopiaexpress123 <basepaxton@gmail.com>
2025-09-05 12:45:45 +0200

    UI: header + térkép komponens

--- app/layout.tsx
+++ app/layout.tsx
@@ -106,17 +106,7 @@ export default function RootLayout({
   <ClerkProvider>
     <html lang="en" className="dark">
       <body className={`font-mono ${GeistSans.variable} ${GeistMono.variable} bg-black text-green-400 antialiased`}>
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-              </SignedOut>
-              <SignedIn>
-              <UserButton />
-            </SignedIn>
+
 
         <div className="bg-video" aria-hidden="true">
           <video
